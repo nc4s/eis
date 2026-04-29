539027dd04a859495aa0ac5e3c17a1748f11ca4f
nc4s <eenmens339@gmail.com>
2026-04-29 16:50:19 +0200

    idc

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
+#include <string.h>
+
 extern void print_string(const char* msg); // print a string and add a newline
+extern void error_string(const char* msg); // do the same as print_string() but print to stderr
 
 int main(int argc, char** argv) {
     for (int i = 0; i < argc; i++) {
-        print_string(argv[i]);
+        if (argv[i] != NULL && argv[i][0] != '\0') print_string(argv[i]);
+        else error_string("Somehow, an argument with a length of ZERO (that's right, 0)\
+was created. I suggest you replace your operating system.");
     }
+
+    if (strcmp(argv[0], "eis") != 0 && strcmp(argv[0], "./eis") != 0) error_string("I don't like your attitude.");
+
     return 0;
 }
--- print_string.s
+++ print_string.s
@@ -1,45 +1,50 @@
-; a function to print a string and then a newline
-
 section .data
     newline db 10
 
 section .text
     global print_string
+    global error_string
 
-print_string:
-    ; Input: rdi contains the pointer to the string (char*)
+; Helper: Writes a string to a specific FD
+; Expects: RDI = File Descriptor, RSI = String Pointer
+_write_string_helper:
+    cld             ; Ensure DF flag is 0 (scan forward)
+    push rdi        ; Save FD
+    push rsi        ; Save String Pointer
+
+    ; 1. Calculate length using 'scasb'
+    mov rdi, rsi    ; RDI must point to the string for scasb
+    xor al, al      ; Search for 0
+    mov rcx, -1     ; Set RCX to max
+    repne scasb     ; Scan until null terminator
     
-    ; Calculate the string length
-    push rdi          ; Save the pointer to the string
-    xor rdx, rdx      ; Clear rdx (we will use this as the length counter)
-    mov rcx, rdi      ; Copy pointer to rcx for iteration
-
-.length_loop:
-    cmp byte [rcx], 0 ; Check if current byte is null
-    je .write_syscall ; If null, jump to write
-    inc rcx           ; Move to next char
-    inc rdx           ; Increment length counter
-    jmp .length_loop
-
-.write_syscall:
-    ; Call sys_write
-    ; rax = 1 (sys_write)
-    ; rdi = 1 (stdout)
-    ; rsi = original pointer (restored from stack)
-    ; rdx = length (calculated above)
+    not rcx
+    dec rcx         ; RCX now holds the length
+    mov rdx, rcx    ; Move length to RDX
     
-    pop rsi           ; Restore the pointer to rsi (buffer)
-    mov rax, 1        ; System call number for sys_write
-    mov rdi, 1        ; File descriptor 1 (stdout)
-    syscall           ; Invoke kernel
+    ; 2. Call sys_write (the string)
+    pop rsi         ; Restore string pointer
+    pop rdi         ; Restore FD
+    mov rax, 1      ; sys_write
+    syscall
 
-    ; Print the newline character
-    mov rax, 1        ; sys_write
-    mov rdi, 1        ; stdout
-    lea rsi, [rel newline]  ; Pointer to the newline character into rsi
-    mov rdx, 1        ; Length of 1 byte
+    ; 3. Print the newline
+    mov rdx, 1
+    lea rsi, [rel newline]
+    mov rax, 1      ; sys_write
     syscall
     
     ret
+
+; Public Functions (The Wrappers)
+print_string:
+    mov rsi, rdi    ; Move the pointer (from RDI) into RSI
+    mov rdi, 1      ; Set FD to stdout
+    jmp _write_string_helper
+
+error_string:
+    mov rsi, rdi    ; Move the pointer (from RDI) into RSI
+    mov rdi, 2      ; Set FD to stderr
+    jmp _write_string_helper
 
 section .note.GNU-stack noalloc noexec nowrite progbits
